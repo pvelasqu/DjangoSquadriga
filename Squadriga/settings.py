47b7bb1fc0dc1b27f95642ea44494ca248150fa1
Pablo Velasquez <Pablov.1994@gmail.com>
2018-01-07 13:43:37 -0500

    changes to the base.html file

--- Squadriga/settings.py
+++ Squadriga/settings.py
@@ -56,7 +56,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR + '/about/templates/about/',
             BASE_DIR + '/templates/',
         ],
         'APP_DIRS': True,
